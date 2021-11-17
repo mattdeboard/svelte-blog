bed2b16e5a1564940fb94f328476a853066407fd
Matt DeBoard <matt.deboard@gmail.com>
2021-11-17 16:33:18 -0600

    Add machinery for converting md to html.

--- src/components/Article.svelte
+++ src/components/Article.svelte
@@ -1,3 +1,15 @@
+<!-- <script lang="ts">
+  import Article from "src/components/Article.svelte";
+  import ArticleList from "src/components/ArticleList/ArticleList.svelte";
+
+	async function fetchPost() {
+		const data = await fetch('/2014/01/14/automatic-changelog-generation-with-git.md').then(
+			(response) => response.text()
+		);
+		return data;
+	};
+	let promise = fetchPost()
+</script> -->
 <script lang="ts">
 	import SvelteMarkdown from 'svelte-markdown';
 	export let title: string;
@@ -9,6 +21,20 @@
 	<h1 class="title">{title}</h1>
 	<p class="date">written on {date}</p>
 	<div class="content">
-		<SvelteMarkdown source={articleBody} />
+		<!-- svelte-ignore missing-declaration -->
+		<SvelteMarkdown
+			source={articleBody}
+			options={{
+				highlight: function (code, lang, callback) {
+					require('pygmentize-bundled-cached')(
+						{ lang: lang, format: 'html' },
+						code,
+						function (err, result) {
+							callback(err, result.toString());
+						}
+					);
+				}
+			}}
+		/>
 	</div>
 </div>
