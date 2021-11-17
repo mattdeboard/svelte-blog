d8adc0bce52761b8a63c9744b62b3e96c21407e8
Matt DeBoard <matt.deboard@gmail.com>
2021-11-17 15:24:36 -0600

    Add base components.

---
+++ src/components/Article.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import SvelteMarkdown from 'svelte-markdown';
+	export let title: string;
+	export let articleBody: string;
+	export let date: string;
+</script>
+
+<div>
+	<h1 class="title">{title}</h1>
+	<p class="date">written on {date}</p>
+	<div class="content">
+		<SvelteMarkdown source={articleBody} />
+	</div>
+</div>
