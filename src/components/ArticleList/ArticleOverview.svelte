8a0068871567919b32c639a6e5a5d797ca9bf763
Matt DeBoard <matt.deboard@gmail.com>
2021-11-19 23:32:34 -0600

    Rename.

---
+++ src/components/ArticleList/ArticleOverview.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	let link = '::link::';
+	let summary = '::summary::';
+	let date = '::date::';
+</script>
+
+<div class="entry-overview">
+	<div class="detail">
+		<h1><a href=".">{link}</a></h1>
+		<!-- if summary -->
+		<div class="summary">{summary}</div>
+		<!-- endif -->
+		<div class="date">{date}</div>
+	</div>
+</div>
