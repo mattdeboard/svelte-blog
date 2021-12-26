0f5edf13cf7202625ee1e3b420db8d7dc754f42a
Matt DeBoard <mdeboard@cloudflare.com>
2021-12-25 19:18:20 -0600

    Remove unused stuff from configs.

--- svelte.config.js
+++ svelte.config.js
@@ -21,12 +21,6 @@ const config = {
         },
       },
     },
-    // adapter: staticAdapter({
-    // 	// default options are shown
-    // 	pages: 'build',
-    // 	assets: 'build',
-    // 	fallback: null
-    // }),
     // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
   },
